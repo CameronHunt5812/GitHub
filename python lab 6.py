823d5a252e4acf9c7d61c184ff5c813ff1347c5f
CameronHunt5812 <s581267@sdudent.roundRoundRock.isd>
2014-10-17 14:51:34 -0500

    grooping diferent math functions

--- python lab 6.py
+++ python lab 6.py
@@ -1,7 +1,21 @@
-print "How many numbers do you want to add?"
-loop = int(raw_input())
-num1 = 0
-for i in range(loop):
-    print "enter a number"
-    num1 = num1 + int(raw_input())
-print "the answer is " + str(num1)
+def add():
+    print "How many numbers do you want to add?"
+    loop = int(raw_input())
+    num1 = 0
+    for i in range(loop):
+        print "enter a number"
+        num1 = num1 + int(raw_input())
+    print "the answer is " + str(num1)
+
+def addList():
+    list1 = []
+    print "how many numbers do you want to add?"
+    num1 = int(raw_input())
+    for i in range(num1):
+        print "enter a number"
+        num2 = int(raw_input())
+        list1.append(num2)
+    print  str(sum(list1))
+
+def factorial():
+    
