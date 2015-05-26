d94e6fd6f4061b333ca92fec9e934500dc9c7884
CameronHunt5812 <s581267@sdudent.roundRoundRock.isd>
2015-05-26 15:47:54 -0500

    trying to print the controles to the skreen

--- 2D_Platformer_WIP.py
+++ 2D_Platformer_WIP.py
@@ -1,13 +1,15 @@
 from Tkinter import *
+
 root = Tk()
 drawpad = Canvas(root, width=400, height=400)
 drawpad.pack()
 
 ground = drawpad.create_rectangle(0, 380, 410, 410, fill = 'green')
-obstical1 = drawpad.create_rectangle(200, 340, 250, 380)
+obstical1 = drawpad.create_rectangle(200, 320, 250, 330)
 obstical2 = drawpad.create_rectangle(120, 300, 180, 310)
-obstical3 = drawpad.create_rectangle(10, 250, 100, 280)
-objects = [ground, obstical1, obstical2, obstical3]
+obstical3 = drawpad.create_rectangle(80, 250, 100, 260)
+obstical4 = drawpad.create_rectangle(130, 230, 160, 250)
+objects = [ground, obstical1, obstical2, obstical3, obstical4]
 
 player = drawpad.create_rectangle(120, 360, 130, 380, fill = 'red')
 ground = False
@@ -15,6 +17,11 @@ airTime = 0
 xDir = 0
 derection = 0
 key = None
+
+def information ():
+    print 'you are the red rectangle'
+    print 'hold A or D to move leaft or right'
+    print 'use "space" to jump'
 
 def KeyStorage (c):
     global key
@@ -27,9 +34,10 @@ def hop(space):
         airTime = -17
         drawpad.move(player, 0, -1)
         ground = False
-        
+
 root.bind("<Key>", KeyStorage)
 root.bind("<space>", hop)
+
 
 def KeyInput(Key):
     global key
@@ -66,7 +74,7 @@ def collisionDetect ():
     global airTime
     global xDir
     temp = False
-    for i in (objects): #Shuld this be for i in range (objects):?
+    for i in (objects):
         px1, py1, px2, py2 = drawpad.coords(player)
         ox1, oy1, ox2, oy2 = drawpad.coords(i)
         if ((px1 > ox1 and px1 < ox2) or (px2 > ox1 and px2 < ox2)):
@@ -77,7 +85,6 @@ def collisionDetect ():
             if (py2 >= oy1) and (py2 < oy2):
                 drawpad.move(player, 0, oy1 - py2)
                 airTime = 0
-                #xDir = 0
                 temp = True
             else:
                 ground = False
@@ -92,13 +99,13 @@ def collisionDetect ():
         if (temp == True):
             ground = True
 
-
 def animate():
     gravity()
     derection = KeyInput(key)
     drawpad.move(player,derection,0)
-    collisionDetect()   
+    collisionDetect()
     drawpad.after(2, animate)
 
+information()
 animate()
 mainloop()
