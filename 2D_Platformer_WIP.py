e0b7f359235815c851b80d5585e04fcd030adff3
CameronHunt5812 <s581267@sdudent.roundRoundRock.isd>
2015-05-28 16:06:04 -0500

    adding structure to display info
    fixed some of the colition detection and added a label to the top to
    display the controles and if you win, the win statement. this will work
    around the erors i was geting before

--- 2D_Platformer_WIP.py
+++ 2D_Platformer_WIP.py
@@ -1,15 +1,23 @@
 from Tkinter import *
 
 root = Tk()
+
+info = Label(root, text = 'info')
+info.pack(side = 'top')
+
 drawpad = Canvas(root, width=400, height=400)
 drawpad.pack()
 
 ground = drawpad.create_rectangle(0, 380, 410, 410, fill = 'green')
-obstical1 = drawpad.create_rectangle(200, 320, 250, 330)
-obstical2 = drawpad.create_rectangle(120, 300, 180, 310)
-obstical3 = drawpad.create_rectangle(80, 250, 100, 260)
-obstical4 = drawpad.create_rectangle(130, 230, 160, 250)
-objects = [ground, obstical1, obstical2, obstical3, obstical4]
+ob1 = drawpad.create_rectangle(200, 280, 250, 340)
+ob2 = drawpad.create_rectangle(120, 320, 180, 340)
+ob3 = drawpad.create_rectangle(30, 250, 100, 280)
+ob4 = drawpad.create_rectangle(100, 200, 160, 230)
+ob5 = drawpad.create_rectangle(300, 150, 340, 160)
+ob6 = drawpad.create_rectangle(180, 200, 240, 220)
+#ob7 = drawpad.create_rectangle(, , , )
+
+objects = [ground, ob1, ob2, ob3, ob4, ob5]
 
 player = drawpad.create_rectangle(120, 360, 130, 380, fill = 'red')
 
@@ -23,8 +31,6 @@ print 'you are the red rectangle'
 print 'hold A or D to move leaft or right'
 print 'use "space" to jump'
 
-
-
 def hop(space):
     global ground
     global airTime
@@ -37,8 +43,12 @@ def KeyStorage (c):
     global key
     key = c.char
 
-root.bind("<Key>", KeyStorage)
-root.bind("<space>", hop)
+def end (iput):
+    root.destroy()
+
+root.bind('<Key>', KeyStorage)
+root.bind('<space>', hop)
+root.bind('<Escape>', end)
 
 def KeyInput(Key):
     global key
@@ -63,29 +73,28 @@ def gravity():
         drawpad.move(player, xDir, 0)
     else:
         yVelosity = 0
-        
+
     if yVelosity >= 8:
         airTime = 14
     drawpad.move(player, 0, yVelosity)
-    
+
 def collisionDetect ():
     global player
     global ground
     global airTime
     global xDir
     temp = False
+    px1, py1, px2, py2 = drawpad.coords(player)
     for i in (objects):
-        px1, py1, px2, py2 = drawpad.coords(player)
         ox1, oy1, ox2, oy2 = drawpad.coords(i)
         if ((px1 > ox1 and px1 < ox2) or (px2 > ox1 and px2 < ox2)):
             if (py1 < oy2) and (py2 > oy2):
                 drawpad.move(player, 0, oy2 - py1)
                 airTime = 0
 
-            if (py2 >= oy1) and (py2 < oy2):
+            if (py2 >= oy1) and (py1 < oy1):
                 drawpad.move(player, 0, oy1 - py2)
                 airTime = 0
-                #xDir = 0
                 temp = True
             else:
                 ground = False
@@ -107,7 +116,5 @@ def animate():
     collisionDetect()
     root.after(2, animate)
 
-#information()
 animate()
 mainloop()
-root.destroy()
